882234a91bc835f51f345cfddce872b8a544a716
rfgava <gava.f.renato@gmail.com>
2020-06-28 19:21:16 -0300

    modal de detalhes de produtos

---
+++ src/Presentation/Nop.Web/Controllers/CardapioOnlineController.cs
@@ -0,0 +1,155 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Security;
+using Nop.Services.Catalog;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Orders;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Nop.Web.Factories;
+using Nop.Web.Framework;
+
+namespace Nop.Web.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class CardapioOnlineController : BasePublicController
+    {
+        #region Fields
+
+        private readonly CatalogSettings _catalogSettings;
+        private readonly IAclService _aclService;
+        private readonly ICustomerActivityService _customerActivityService;
+        private readonly ILocalizationService _localizationService;
+        private readonly IPermissionService _permissionService;
+        private readonly IProductModelFactory _productModelFactory;
+        private readonly IProductService _productService;
+        private readonly IRecentlyViewedProductsService _recentlyViewedProductsService;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IStoreContext _storeContext;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IWorkContext _workContext;
+        private readonly ShoppingCartSettings _shoppingCartSettings;
+
+        #endregion
+
+        #region Ctor
+
+        public CardapioOnlineController(CaptchaSettings captchaSettings,
+            CatalogSettings catalogSettings,
+            IAclService aclService,
+            ICustomerActivityService customerActivityService,
+            ILocalizationService localizationService,
+            IPermissionService permissionService,
+            IProductModelFactory productModelFactory,
+            IProductService productService,
+            IRecentlyViewedProductsService recentlyViewedProductsService,
+            IShoppingCartService shoppingCartService,
+            IStoreContext storeContext,
+            IStoreMappingService storeMappingService,
+            IUrlRecordService urlRecordService,
+            IWorkContext workContext,
+            ShoppingCartSettings shoppingCartSettings)
+        {
+            _catalogSettings = catalogSettings;
+            _aclService = aclService;
+            _customerActivityService = customerActivityService;
+            _localizationService = localizationService;
+            _permissionService = permissionService;
+            _productModelFactory = productModelFactory;
+            _productService = productService;
+            _recentlyViewedProductsService = recentlyViewedProductsService;
+            _shoppingCartService = shoppingCartService;
+            _storeContext = storeContext;
+            _storeMappingService = storeMappingService;
+            _urlRecordService = urlRecordService;
+            _workContext = workContext;
+            _shoppingCartSettings = shoppingCartSettings;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult GetProductDetails(int productId, int updatecartitemid = 0)
+        {
+            var product = _productService.GetProductById(productId);
+            if (product == null || product.Deleted)
+                return InvokeHttp404();
+
+            var notAvailable =
+                //published?
+                (!product.Published && !_catalogSettings.AllowViewUnpublishedProductPage) ||
+                //ACL (access control list) 
+                !_aclService.Authorize(product) ||
+                //Store mapping
+                !_storeMappingService.Authorize(product) ||
+                //availability dates
+                !_productService.ProductIsAvailable(product);
+            //Check whether the current user has a "Manage products" permission (usually a store owner)
+            //We should allows him (her) to use "Preview" functionality
+            var hasAdminAccess = _permissionService.Authorize(StandardPermissionProvider.AccessAdminPanel) && _permissionService.Authorize(StandardPermissionProvider.ManageProducts);
+            if (notAvailable && !hasAdminAccess)
+                return InvokeHttp404();
+
+            //visible individually?
+            if (!product.VisibleIndividually)
+            {
+                //is this one an associated products?
+                var parentGroupedProduct = _productService.GetProductById(product.ParentGroupedProductId);
+                if (parentGroupedProduct == null)
+                    return RedirectToRoute("Homepage");
+
+                return RedirectToRoutePermanent("Product", new { SeName = _urlRecordService.GetSeName(parentGroupedProduct) });
+            }
+
+            //update existing shopping cart or wishlist  item?
+            ShoppingCartItem updatecartitem = null;
+            if (_shoppingCartSettings.AllowCartItemEditing && updatecartitemid > 0)
+            {
+                var cart = _shoppingCartService.GetShoppingCart(_workContext.CurrentCustomer, storeId: _storeContext.CurrentStore.Id);
+                updatecartitem = cart.FirstOrDefault(x => x.Id == updatecartitemid);
+                //not found?
+                if (updatecartitem == null)
+                {
+                    return RedirectToRoute("Product", new { SeName = _urlRecordService.GetSeName(product) });
+                }
+                //is it this product?
+                if (product.Id != updatecartitem.ProductId)
+                {
+                    return RedirectToRoute("Product", new { SeName = _urlRecordService.GetSeName(product) });
+                }
+            }
+
+            //save as recently viewed
+            _recentlyViewedProductsService.AddProductToRecentlyViewedList(product.Id);
+
+            //display "edit" (manage) link
+            if (_permissionService.Authorize(StandardPermissionProvider.AccessAdminPanel) &&
+                _permissionService.Authorize(StandardPermissionProvider.ManageProducts))
+            {
+                //a vendor should have access only to his products
+                if (_workContext.CurrentVendor == null || _workContext.CurrentVendor.Id == product.VendorId)
+                {
+                    DisplayEditLink(Url.Action("Edit", "Product", new { id = product.Id, area = AreaNames.Admin }));
+                }
+            }
+
+            //activity log
+            _customerActivityService.InsertActivity("PublicStore.ViewProduct",
+                string.Format(_localizationService.GetResource("ActivityLog.PublicStore.ViewProduct"), product.Name), product);
+
+            //model
+            var model = _productModelFactory.PrepareProductDetailsModel(product, updatecartitem, false);
+
+            return PartialView("_ProductDetails", model);
+        }
+
+
+
+    }
+}
---
+++ src/Presentation/Nop.Web/Themes/CardapioOnline/Content/js/product-details-modal.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    // This is need so the content gets replaced correctly.
+    $("#productdetails-window").on("show.bs.modal", function (e) {
+
+        var productId = e.relatedTarget.attributes["data-productid"].value;
+
+        $.ajax({
+            cache: false,
+            type: "GET",
+            url: "CardapioOnline/GetProductDetails?productId=" + productId,
+            success: function (response) {
+                $(".modal-content").html(response);
+            },
+            error: function () {
+                alert('error');
+            }
+        });
+    });
+
+    $("#productdetails-window").on("hide.bs.modal", function (e) {
+
+        $(".modal-content").html("<div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button><h4>Carregando, por favor aguarde...</h4></div><div class=\"modal-body\"><div class=\"modal-busy\"><span>&nbsp;</span></div></div>");
+    });
+});
